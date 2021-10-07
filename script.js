1a88cc777559420dfdbdc487f57b9386cc7a56c3
SharonAbra <abra.sharon@gmail.com>
2021-10-07 23:20:56 +0300

    b

--- script.js
+++ script.js
@@ -6,22 +6,3 @@ button.addEventListener('click', () => {
         behavior: 'smooth' 
     });
 })
-
-// const slideShow = document.querySelector('.carousel-fade');
-// const hidden = document.querySelectorAll('.hide');
-// slideShow.addEventListener('scroll', function () {
-//     hidden.map(item => {
-//         item.classList.remove('hide');
-//     })
-// });
-
-// const hidden = document.querySelectorAll('.hide');
-// window.onscroll = function() {myFunction()};
-
-// function myFunction() {
-//   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-//     hidden.map(item => {
-//         item.classList.remove('hide');
-//     })
-//   }
-// }
