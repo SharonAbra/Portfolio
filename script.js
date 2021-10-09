efd13dd470ad6584fdb428f76e203b46c8245fe5
SharonAbra <abra.sharon@gmail.com>
2021-10-09 15:30:28 +0300

    contact

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const button = document.querySelector(".take-a-look");
 const about = document.querySelector(".about");
+const contact = document.querySelector(".contact-button");
+const modal = document.querySelector(".modal-dialog");
 
 button.addEventListener('click', () => {
     about.scrollIntoView({ 
         behavior: 'smooth' 
     });
 })
+
+// contact.addEventListener('click', ()=> {
+
+// })
