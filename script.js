736de6dc4706bb7ca0a4743a5e2cd4267e3c4ea1
SharonAbra <abra.sharon@gmail.com>
2021-10-10 11:55:49 +0300

    cards

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const button = document.querySelector(".take-a-look");
 const about = document.querySelector(".about");
-const contact = document.querySelector(".contact-button");
-const modal = document.querySelector(".modal-dialog");
 
 button.addEventListener('click', () => {
     about.scrollIntoView({ 
         behavior: 'smooth' 
     });
 })
-
-// contact.addEventListener('click', ()=> {
-
-// })
