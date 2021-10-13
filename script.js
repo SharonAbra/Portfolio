990252a15d9ac7d2cfd0743da82c4bf21c656d41
SharonAbra <abra.sharon@gmail.com>
2021-10-13 11:41:19 +0300

    css added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const button = document.querySelector(".take-a-look");
+const takeAlook = document.querySelector(".take-a-look");
 const about = document.querySelector(".about");
+const seeMore = document.querySelector(".see-more-button");
+const moreProjects = document.querySelector(".more-projects");
 
-button.addEventListener('click', () => {
+takeAlook.addEventListener('click', () => {
     about.scrollIntoView({ 
         behavior: 'smooth' 
     });
+})
+
+seeMore.addEventListener('click', () => {
+    if (moreProjects.classList.contains("hide")) {
+        moreProjects.classList.remove("hide");
+        seeMore.innerHTML = "See Less";
+    } else {
+        moreProjects.classList.add("hide");
+        seeMore.innerHTML = "See More";
+    }
 })
